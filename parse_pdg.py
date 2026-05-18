6dad75e965958e463efc23396027890c240c797a
DomenicoAnzalone <anzalone.domenico07@gmail.com>
2026-05-18 21:03:54 +0200

    Add private repository list and update repository handling in scripts
    - Introduced a new file to list private repositories.
    - Updated `parse_pdg.py` to filter out private repositories from the repository list.
    - Modified `project_pdg_info.py` to streamline repository cloning and handle existing directories.
    - Enhanced `clone.py` to skip cloning if the repository already exists locally.
    - Updated `requirements.txt` to ensure all necessary packages are included.

--- parse_pdg.py
+++ parse_pdg.py
@@ -2,6 +2,8 @@ import pandas as pd
 import re
 import os
 
+PRIVATE_REPO = {'openstack-ops', 'ansible-rabbitmq', 'PGBlitz'}
+
 def getRepoDictionary():
     data = pd.read_csv(os.path.normpath(os.path.join(os.getcwd(), "input", "ansible.csv")))
     repositories = data.repository
@@ -9,6 +11,9 @@ def getRepoDictionary():
     githubName_repoName = repositories
     # remove github username from file path
     pattern = re.compile(r'^.*?/')
+    for repo in PRIVATE_REPO:
+        if repo in githubName_repoName:
+            githubName_repoName.remove(repo)
     repoDic = {}
     for string in githubName_repoName:
         repoDic[pattern.sub('', string)] = string
